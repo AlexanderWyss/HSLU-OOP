d2c0a9c3fd1e86c79f964db09b6f01a589bc765a
Alexander Wyss <alexsilvan.wyss@gmail.com>
2021-03-29 14:05:37 +0200

    sw06 point moveRelative

--- src/main/java/aufgaben/Point.java
+++ src/main/java/aufgaben/Point.java
@@ -1,12 +1,22 @@
 package aufgaben;
 
+import static java.lang.Math.*;
+
 class Point {
-    private final int x;
-    private final int y;
+    private int x;
+    private int y;
 
     public Point(final int x, final int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     public int getQuadrant() {
@@ -20,5 +30,20 @@ class Point {
             return 4;
         }
         return 0;
+    }
+
+    public void moveRelative(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    public void moveRelative(Point point) {
+        moveRelative(point.getX(), point.getY());
+    }
+
+    public void moveRelativeAngle(double angle, double value) {
+        final double radians = toRadians(angle);
+        this.x += round(cos(radians) * value);
+        this.y += round(sin(radians) * value);
     }
 }
--- src/test/java/aufgaben/PointTest.java
+++ src/test/java/aufgaben/PointTest.java
@@ -5,6 +5,119 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
+
+    // sw06
+    @Test
+    void a_point_moveRelativeXY_xAndYCorrect() {
+        Point point = new Point(3, 5);
+
+        point.moveRelative(6, 2);
+
+        assertEquals(9, point.getX());
+        assertEquals(7, point.getY());
+    }
+
+    @Test
+    void a_point_moveRelativeNegativeXY_xAndYCorrect() {
+        Point point = new Point(3, 5);
+
+        point.moveRelative(-6, -2);
+
+        assertEquals(-3, point.getX());
+        assertEquals(3, point.getY());
+    }
+
+    @Test
+    void a_pointWithNegativeXY_moveRelativeXY_xAndYCorrect() {
+        Point point = new Point(-3, -5);
+
+        point.moveRelative(-6, 7);
+
+        assertEquals(-9, point.getX());
+        assertEquals(2, point.getY());
+    }
+
+    @Test
+    void a_pointZeroXY_moveRelativeXY_xAndYCorrect() {
+        Point point = new Point(0, 0);
+
+        point.moveRelative(-6, 7);
+
+        assertEquals(-6, point.getX());
+        assertEquals(7, point.getY());
+    }
+
+    @Test
+    void a_point_moveRelativeZeroXY_xAndYCorrect() {
+        Point point = new Point(4, -5);
+
+        point.moveRelative(0, 0);
+
+        assertEquals(4, point.getX());
+        assertEquals(-5, point.getY());
+    }
+
+    @Test
+    void b_point_moveRelative_xAndYCorrect() {
+        Point point = new Point(3, 5);
+
+        point.moveRelative(new Point(6, 2));
+
+        assertEquals(9, point.getX());
+        assertEquals(7, point.getY());
+    }
+
+    @Test
+    void c_point_moveRelativeAngle_xAndYCorrect() {
+        Point point = new Point(3, 5);
+
+        point.moveRelativeAngle(35.7, 4);
+
+        assertEquals(6, point.getX());
+        assertEquals(7, point.getY());
+    }
+
+    @Test
+    void c_pointNegativeXY_moveRelativeAngle_xAndYCorrect() {
+        Point point = new Point(-3, -5);
+
+        point.moveRelativeAngle(49.2, 2);
+
+        assertEquals(-2, point.getX());
+        assertEquals(-3, point.getY());
+    }
+
+    @Test
+    void c_pointNegativeXPositiveY_moveRelativeAngle_xAndYCorrect() {
+        Point point = new Point(-3, 5);
+
+        point.moveRelativeAngle(60.1, 7);
+
+        assertEquals(0, point.getX());
+        assertEquals(11, point.getY());
+    }
+
+    @Test
+    void c_point_moveRelativeNegativeAngle_xAndYCorrect() {
+        Point point = new Point(3, 5);
+
+        point.moveRelativeAngle(-220.1, 9);
+
+        assertEquals(-4, point.getX());
+        assertEquals(11, point.getY());
+    }
+
+    @Test
+    void c_point_moveRelativeAngleNegativeValue_xAndYCorrect() {
+        Point point = new Point(3, 5);
+
+        point.moveRelativeAngle(195.1, -4);
+
+        assertEquals(7, point.getX());
+        assertEquals(6, point.getY());
+    }
+
+    // sw04
     @Test
     void h_getQuadrant_1() {
         assertEquals(1, new Point(1, 1).getQuadrant());
