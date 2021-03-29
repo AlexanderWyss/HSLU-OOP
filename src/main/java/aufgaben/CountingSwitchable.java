3ce7a2153eb7d105c36b62e2fbbeca8b6c652bd9
Alexander Wyss <alexsilvan.wyss@gmail.com>
2021-03-29 14:40:13 +0200

    sw05 CountingSwitchable

---
+++ src/main/java/aufgaben/CountingSwitchable.java
@@ -0,0 +1,33 @@
+package aufgaben;
+
+public class CountingSwitchable implements Switchable {
+    private boolean value = false;
+    private int count = 0;
+
+
+    @Override
+    public void switchOn() {
+        value = true;
+        count++;
+    }
+
+    @Override
+    public void switchOff() {
+        value = false;
+        count++;
+    }
+
+    @Override
+    public boolean isSwitchedOn() {
+        return value;
+    }
+
+    @Override
+    public boolean isSwitchedOff() {
+        return !value;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
