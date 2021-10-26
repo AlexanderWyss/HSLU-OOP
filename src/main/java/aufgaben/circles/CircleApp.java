6414f98ff8b2fe8129b89064f990b3d03f9d5614
Alexander Wyss <alexsilvan.wyss@gmail.com>
2021-10-26 12:24:01 +0200

    circle app

--- src/main/java/aufgaben/circles/CircleApp.java
+++ src/main/java/aufgaben/circles/CircleApp.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 
 public class CircleApp {
     public static void main(String[] args) {
-        CircleCanvas circleCanvas = new CircleCanvas();
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
+        CircleCanvas circleCanvas = new CircleCanvas();
         frame.add(circleCanvas);
         new DaemonThread(circleCanvas).start();
         frame.setVisible(true);
